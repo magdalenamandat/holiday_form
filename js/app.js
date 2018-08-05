6879b72fd82fa575f38109657f86f4d5e5966b67
Magdalena <m.mandat@gmail.com>
2018-08-05 15:59:51 +0100

    rating works

---
+++ js/app.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const commentList = document.querySelector('#comment-list');
+  const form = document.querySelector('#new-form');
+  const deleteButton = document.querySelector('#delete-all');
+
+
+  const dealWithForm = function (event) {
+    event.preventDefault();
+    const form = event.target;
+
+    const nameInput = form.name.value;
+    const nameFilled = `Name: ${nameInput}`;
+    const name = document.createElement('form');
+    name.textContent = nameFilled;
+    commentList.append(name);
+
+    // const bookingInput = form.booking.value;
+    // const bookingField = `Booking number: ${bookingInput}`;
+    // const booking = document.createElement('form');
+    // booking.textContent = bookingFilled;
+    // commentList.append(booking);
+
+
+    const ratingInput = form.rating.value;
+    const ratingFilled = `Rating: ${ratingInput}`;
+    const rating = document.createElement('form');
+    rating.textContent = ratingFilled;
+    commentList.append(rating);
+
+    const commentInput = form.comment.value;
+    const commentFilled = `Comment: ${commentInput}`;
+    const comment = document.createElement('form');
+    comment.textContent = commentFilled;
+    commentList.append(comment);
+
+    const line = document.createElement('hr');
+    commentList.append(line);
+    form.reset();
+
+  }
+
+  const resetForm = function (event) {
+    commentList.innerHTML = "previous comments have been deleted";
+  }
+
+  form.addEventListener('submit', dealWithForm);
+  deleteButton.addEventListener('click', resetForm);
+})
